ce89f1bccae8535cf8ddc5d306dcb9a48ea097a3
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-09-16 11:52:26 +0200

    Update build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,33 @@
 plugins {
-  id("org.metaborg.gradle.config.root-project") version "0.4.7"
-  id("org.metaborg.gradle.config.java-library") version "0.4.7"
-  id("org.metaborg.gitonium") version "0.1.5" // Bootstrap with previous version.
-  kotlin("jvm") version "1.3.41" // 1.3.41 in sync with kotlin-dsl plugin.
-  `kotlin-dsl`
-  `java-gradle-plugin`
-  `maven-publish`
+    `java-library`
+    `kotlin-dsl`
+    `java-gradle-plugin`
+    `maven-publish`
+    alias(libs.plugins.gitonium.bootstrap)   // Bootstrap with previous version.
+    alias(libs.plugins.kotlin.jvm)
 }
 
-kotlinDslPluginOptions {
-  experimentalWarning.set(false)
-}
-
-gradlePlugin {
-  plugins {
-    create("gitonium") {
-      id = "org.metaborg.gitonium"
-      implementationClass = "mb.gitonium.GitoniumPlugin"
-    }
-  }
+repositories {
+    mavenCentral()
 }
 
 dependencies {
-  implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
+    implementation(libs.jgit)
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+gradlePlugin {
+    plugins {
+        create("gitonium") {
+            id = "org.metaborg.gitonium"
+            implementationClass = "mb.gitonium.GitoniumPlugin"
+        }
+    }
 }
